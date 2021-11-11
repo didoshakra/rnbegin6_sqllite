a125cbf82aa5f838908a9917439019ece7956326
Roman <didoshakr@ukr.net>
2021-11-11 18:51:51 +0200

    CreateSQLite table

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
+import {openDatabase} from 'react-native-sqlite-storage';
+var db = openDatabase({name: 'SchoolDatabase.db'});
 
 const App = () => {
-  return (
-    <View>
-      <Text style={{textAlign:'center',fontSize:22,color: 'red'}}>ReactNative SQLlite</Text>
-    </View>
-  )
-}
+  const createTable = () => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Student_Table'",
+        [],
+        function (tx, res) {
+          console.log('item:', res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Student_Table', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS Student_Table(student_id INTEGER PRIMARY KEY AUTOINCREMENT, student_name VARCHAR(30), student_phone INT(15), student_address VARCHAR(255))',
+              [],
+            );
+          }
+        },
+      );
+    });
+    Alert.alert('SQLite Database and Table Successfully Created...');
+  };
 
-export default App
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text style={{textAlign: 'center', fontSize: 22, color: 'red'}}>
+        ReactNative SQLlite
+      </Text>
+      <View style={styles.mainContainer}>
+        <TouchableOpacity style={styles.touchableOpacity} onPress={createTable}>
+          <Text style={styles.touchableOpacityText}>
+            Click Here To Create SQLite Database and Table in React Native{' '}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+
+  touchableOpacity: {
+    backgroundColor: '#1B5E20',
+    alignItems: 'center',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+  touchableOpacityText: {
+    color: '#FFFFFF',
+    fontSize: 23,
+    textAlign: 'center',
+    padding: 8,
+  },
+});
+
+export default App;
---
+++ readme.txt
@@ -0,0 +1,6 @@
+//11.11.2021 rnbegin6_sqllite //
+//16.06.2021 Using SQLite with React Native //https://blog.logrocket.com/using-sqlite-with-react-native/
+//28.12.2020 Example of Creating SQLite Database Table in React Native
+https://reactnativecode.com/creating-sqlite-database-table-in-react-native/
+=============================================================================
+npm install --save react-native-sqlite-storage
