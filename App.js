cf27bec450eead77a6ce65c32861db54d8b7c0ac
Roman <didoshakr@ukr.net>
2021-11-11 18:18:06 +0200

    begin

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{textAlign:'center',fontSize:22,color: 'red'}}>ReactNative SQLlite</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ android/app/src/main/java/com/rnbegin6_sqllite/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rnbegin6_sqllite;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rnbegin6_sqllite";
+  }
+}
